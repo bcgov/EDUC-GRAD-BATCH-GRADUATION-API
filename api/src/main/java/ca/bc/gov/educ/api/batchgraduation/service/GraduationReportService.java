44a0c89872bac2c66dc7e3f3fec1226b3032eb79
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2022-08-16 10:37:22 -0700

    GRAD2-1424

--- api/src/main/java/ca/bc/gov/educ/api/batchgraduation/service/GraduationReportService.java
+++ api/src/main/java/ca/bc/gov/educ/api/batchgraduation/service/GraduationReportService.java
@@ -1,7 +1,6 @@
 package ca.bc.gov.educ.api.batchgraduation.service;
 
 import ca.bc.gov.educ.api.batchgraduation.model.PsiCredentialDistribution;
-import ca.bc.gov.educ.api.batchgraduation.model.SchoolReportDistribution;
 import ca.bc.gov.educ.api.batchgraduation.model.StudentCredentialDistribution;
 import ca.bc.gov.educ.api.batchgraduation.util.EducGradBatchGraduationApiConstants;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,17 +34,6 @@ public class GraduationReportService {
 
 	public Mono<List<StudentCredentialDistribution>> getCertificateList(String accessToken) {
 		return webClient.get().uri(constants.getCertificateDistributionList()).headers(h -> h.setBearerAuth(accessToken)).retrieve().bodyToMono(new ParameterizedTypeReference<List<StudentCredentialDistribution>>(){});
-	}
-
-	public List<SchoolReportDistribution> getSchoolReportForPosting(String accessToken) {
-		UUID correlationID = UUID.randomUUID();
-		final ParameterizedTypeReference<List<SchoolReportDistribution>> responseType = new ParameterizedTypeReference<>() {
-		};
-		return webClient.get().uri(constants.getSchoolReportPostingList())
-				.headers(h -> {
-					h.setBearerAuth(accessToken);
-					h.set(EducGradBatchGraduationApiConstants.CORRELATION_ID, correlationID.toString());
-				}).retrieve().bodyToMono(responseType).block();
 	}
 
 	public List<PsiCredentialDistribution> getPsiStudentsForRun(String transmissionType,String psiCode,String psiYear,String accessToken) {
