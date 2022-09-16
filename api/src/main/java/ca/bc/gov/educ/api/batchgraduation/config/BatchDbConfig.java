e3bfc56bc02cbe50402d683776c7f7de64d8295b
Jinil Sung <infstar@gmail.com>
2022-09-15 22:52:59 -0700

    Update BatchDbConfig.java
    Tested maxLifeTime set larger than the actual database timeout limit.

--- api/src/main/java/ca/bc/gov/educ/api/batchgraduation/config/BatchDbConfig.java
+++ api/src/main/java/ca/bc/gov/educ/api/batchgraduation/config/BatchDbConfig.java
@@ -70,7 +70,7 @@ public class BatchDbConfig {
 
         config.setMinimumIdle(15);
         config.setMaximumPoolSize(maxPoolSize);
-        config.setMaxLifetime(maxLifetime);
+        config.setMaxLifetime(18000000);
         config.setConnectionTimeout(connectionTimeout);
         config.setKeepaliveTime(60000); // 1 minute
         config.setIdleTimeout(120000);  // 2 minutes
