cee44f87baef416abf9cb35835d0e567db67cdfb
arybakov <alex.rybakov@cgi.com>
2023-07-19 09:46:28 -0600

    Added filter

--- api/src/main/java/ca/bc/gov/educ/api/batchgraduation/reader/DistributionRunYearlyPartitioner.java
+++ api/src/main/java/ca/bc/gov/educ/api/batchgraduation/reader/DistributionRunYearlyPartitioner.java
@@ -1,8 +1,11 @@
 package ca.bc.gov.educ.api.batchgraduation.reader;
 
+import ca.bc.gov.educ.api.batchgraduation.model.District;
 import ca.bc.gov.educ.api.batchgraduation.model.StudentCredentialDistribution;
+import ca.bc.gov.educ.api.batchgraduation.model.StudentSearchRequest;
 import ca.bc.gov.educ.api.batchgraduation.rest.RestUtils;
 import ca.bc.gov.educ.api.batchgraduation.service.ParallelDataFetch;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.JobExecution;
@@ -10,6 +13,7 @@ import org.springframework.batch.item.ExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,6 +41,20 @@ public class DistributionRunYearlyPartitioner extends BasePartitioner {
         restUtils.deleteSchoolReportRecord("", "DISTREP_YE_SD", restUtils.getAccessToken());
 
         List<StudentCredentialDistribution> credentialList = parallelDataFetch.fetchStudentCredentialsDistributionDataYearly();
+        StudentSearchRequest searchRequest = getStudentSearchRequest();
+        if(searchRequest != null && searchRequest.getSchoolCategoryCodes() != null && !searchRequest.getSchoolCategoryCodes().isEmpty()) {
+            List<String> useFilterDistricts = new ArrayList<>();
+            for(String schoolCategoryCode: searchRequest.getSchoolCategoryCodes()) {
+                List<District> districts = restUtils.getDistrictBySchoolCategoryCode(schoolCategoryCode);
+                for(District district: districts) {
+                    useFilterDistricts.add(district.getDistrictNumber());
+                }
+            }
+            credentialList.removeIf(scr->!useFilterDistricts.contains(StringUtils.substring(scr.getSchoolOfRecord(), 0, 3)));
+        }
+        if(searchRequest != null && searchRequest.getDistricts() != null && !searchRequest.getDistricts().isEmpty()) {
+            credentialList.removeIf(scr->!searchRequest.getDistricts().contains(StringUtils.substring(scr.getSchoolOfRecord(), 0, 3)));
+        }
         if(!credentialList.isEmpty()) {
             updateBatchJobHistory(createBatchJobHistory(), (long) credentialList.size());
             return getStringExecutionContextMap(gridSize, credentialList, null, LOGGER);
