0565538b7f620be287fd61717a9ec2866428faa8
arybakov <alex.rybakov@cgi.com>
2024-06-11 11:27:45 -0600

    GRAD2-2758 YE Distribution Run to Revert the bundling back to SCHOOL OF RECORD

--- api/src/main/java/ca/bc/gov/educ/api/batchgraduation/processor/DistributionRunProcessor.java
+++ api/src/main/java/ca/bc/gov/educ/api/batchgraduation/processor/DistributionRunProcessor.java
@@ -3,7 +3,6 @@ package ca.bc.gov.educ.api.batchgraduation.processor;
 import ca.bc.gov.educ.api.batchgraduation.model.DistributionSummaryDTO;
 import ca.bc.gov.educ.api.batchgraduation.model.StudentCredentialDistribution;
 import ca.bc.gov.educ.api.batchgraduation.rest.RestUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
@@ -29,10 +28,12 @@ public class DistributionRunProcessor implements ItemProcessor<StudentCredential
 		LOGGER.info("Processing partitionData = {}", item.getCredentialTypeCode());
 		summaryDTO.setBatchId(batchId);
 		boolean useSchoolAtGrad = false;
+		// Revert code back to school of record GRAD2-2758
+		/**
 		String credentialType = summaryDTO.getCredentialType();
 		if (StringUtils.equalsAnyIgnoreCase(credentialType, "OC", "RC")) {
 			useSchoolAtGrad = true;
-		}
+		} **/
 		return restUtils.processDistribution(item, summaryDTO, useSchoolAtGrad);
 		
 	}
