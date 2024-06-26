adc001f74ce36ac681fcc63e37a0722a204e0db0
githubmamatha <harmainimamatha@gmail.com>
2024-06-26 14:13:58 -0700

    Grad2-2820 new batch job type code added

---
+++ api/src/main/resources/db/migration/1.0/V1.0.49__DML-UPDATE_DATA-BATCH_JOB_TYPE_CODE.sql
@@ -0,0 +1,2 @@
+INSERT INTO BATCH_JOB_TYPE_CODE (BATCH_JOB_TYPE_CODE,LABEL,DESCRIPTION,DISPLAY_ORDER,EFFECTIVE_DATE,EXPIRY_DATE)
+VALUES ('SCHL_RPT_REGEN','User Request School Report Regeneration','The User regenerate School Reports option allows Users to manually update school reports upon demand.',130,TIMESTAMP'2024-06-26  00:00:00.0',NULL);
